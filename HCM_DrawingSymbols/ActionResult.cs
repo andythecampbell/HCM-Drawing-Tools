30f3d03f4e3e50bef29b74266b79e3626c866f4b
Andy Campbell <andycampbell@d3tech.net>
2018-03-31 22:25:00 -0600

    additional class missed from previous commit

---
+++ HCM_DrawingSymbols/ActionResult.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HCMToolsInventorAddIn
+{
+    public class ActionResult
+    {
+        public ActionResult(bool success)
+        {
+            Success = success;
+        }
+        public ActionResult(bool success, string message)
+        {
+            Success = success;
+            Message = message;
+        }
+
+        public ActionResult(Exception exception)
+        {
+            Success = false;
+            Exception = exception;
+            Message = exception.Message;
+        }
+
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public Exception Exception { get; set; }
+    }
+}
